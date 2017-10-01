dc2869e79616cd13ed3d448b420a19e5139e778d
Anifowose <Quadriyanney@gmail.com>
2017-10-01 10:54:06 +0100

    modified app.py

--- src/app.py
+++ src/app.py
@@ -177,4 +177,5 @@ def logout():
 
 if __name__=='__main__':
 	app.secret_key = 'ljfdnvljdsna;/ds.lkdfkjnmkdfuworiorip324u84387932oibi23j0i3nj'
-	app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
